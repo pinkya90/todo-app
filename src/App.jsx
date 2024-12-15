4509adafb0ecd85b053471729b874f3467130a3d
pinkya90 <pinks.a90@gmail.com>
2024-12-15 13:48:25 +0530

    edited todolist import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState , useEffect} from "react";
 import { Header } from "./components/Header";
 import { Tabs } from "./components/Tabs";
 import { TodoInput } from "./components/TodoInput";
-import { TodoList } from "./components/Todolist";
+import { TodoList } from "./components/TodoList";
 
 
 function App() {
